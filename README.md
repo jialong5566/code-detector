c9b9c10e439c1172ed3a3c8d0f1f6ca11984ca3f
贾龙 <john17@sfmail.sf-express.com>
2025-09-04 20:29:13 +0800

    feat: rewrite fn of git diff

--- README.md
+++ README.md
@@ -1,21 +1,19 @@
 # js-code-detector
 
 ## 说明
-1.重复代码检测工具, 支持 React、Vue 项目
-2.git分支对比报告, 目前适用于 umi4.x 版本的项目
+- 重复代码检测工具, 支持 React、Vue 项目
+- git分支对比报告, 目前适用于 umi4.x 版本的项目
 
 
 ## 重复代码检测使用
-
-1.项目安装 **js-code-detector**
-2.package.json 中添加 scripts 命令 ```sameCodeDetect```, 例如：```"same": "sameCodeDetect"```
-3.执行该命令可在当前项目目录下生成报告, ```npm run same```
+- 项目安装 **js-code-detector**
+- package.json 中添加 scripts 命令 ```sameCodeDetect```, 例如：```"same": "sameCodeDetect"```
+- 执行该命令可在当前项目目录下生成报告, ```npm run same```
 
 ## git分支对比报告使用
-
-1.项目安装 **js-code-detector**
-2.package.json 中添加 scripts 命令 ```detect```, 例如：```"detect": "detect"```
-3.执行该命令可在当前项目目录下生成报告, ```npm run detect```
+- 项目安装 **js-code-detector**
+- package.json 中添加 scripts 命令 ```gitDiffDetect```, 例如：```"detect": "gitDiffDetect"```
+- 执行该命令可在当前项目目录下生成报告, ```npm run detect```
 
 ## 重复代码检测报告说明
 1.所有相似的代码片段为一组，每组有2个及以上的代码片段
@@ -23,17 +21,17 @@
 
 
 ## git分支对比报告说明
-- filePath 文件路径
-- type 改动类型 add remove modify
-- filesDependsOnMe 依赖当前文件的文件
-- dangerIdentifiers 危险标识符（包括 1、找不到定义的， 2、函数的参数是一个函数，并进行调用的）
-- blockReports 区块报告（每个diff块产生一个报告）
-  - kind 区块类型，目前把重点的类型单独提取处理，包括 "Import"|"Declaration"|"Assignment"|"SelfUpdate"|"Invoke"
-  - diff 区块的原始改动内容
-  - added 增加的标识符
-  - addedEffects 增加的标识符的影响
-    - causeBy 增加的标识符的来源
-    - effects 影响到的标识符的位置
+- 文件路径
+- 改动类型 add remove modify
+- 依赖当前文件的文件
+- 危险标识符（包括 1、找不到定义的， 2、函数的参数是一个函数，并进行调用的）
+- 区块报告（每个diff块产生一个报告）
+  - 区块类型，目前把重点的类型单独提取处理，包括 "Import"|"Declaration"|"Assignment"|"SelfUpdate"|"Invoke"
+  - 区块的原始改动内容
+  - 增加的标识符
+  - 增加的标识符的影响
+    - 增加的标识符的来源
+    - 影响到的标识符的位置
 
 
 ## LICENSE
--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import {formatGitDiffContent} from "./util/format_git_diff_content";
 import madge_util from "./util/madge_util";
 import {writeFileSync} from "fs";
 import {createDetectReport} from "./util/report_util";
-import {execa, chalk} from "@umijs/utils";
+import {execa, chalk, logger, rimraf} from "@umijs/utils";
 import dayjs from "dayjs";
 import {createMdByJson} from "./util/report_util/createMdByJson";
 import {readSrcFiles} from "./util/shared/readDirFiles";
@@ -47,14 +47,14 @@ git clone $1 source
 `;
 const pluginFileContent = `import * as cb from "js-code-detector"
 
-export default async (api) => {
+export default async (api: any) => {
   const buildCallback = () => cb.umiPluginCallback(api);
   api.onBuildComplete(buildCallback);
 }`
 export async function writeGitDiffTxt(gitUrl: string, branchName: string){
   const today = dayjs().format('YYYYMDD');
   writeFileSync(join(process.cwd(), 'detect.sh'), shellFileContent, { encoding: 'utf-8', flag: 'w' });
-  execa.execa('chmod +x detect.sh', {shell: '/bin/bash'});
+  await execa.execa('chmod +x detect.sh', {shell: '/bin/bash'});
   const res0 = await execa.execa('sh detect.sh', [gitUrl, branchName], {shell: '/bin/bash'})
   chalk.green(["临时文件夹建立，源代码clone完成"]);
   writeFileSync(join(process.cwd(), today, 'target', 'plugin.ts'), pluginFileContent, { encoding: 'utf-8', flag: 'w' });
@@ -75,8 +75,8 @@ export async function getGitRepositoryAndBranch(){
   }
 }
 
-export function generateReport(jsonStr: string){
-  writeFileSync(join(process.cwd(), `${dayjs().format('YYYYMDD_HHmm')}_${jsonName}`), jsonStr, { encoding: 'utf-8', flag: 'w' });
+export function generateReport(content: string){
+  writeFileSync(join(process.cwd(), `${dayjs().format('YYYYMDD_HHmm')}_${jsonName}`), content, { encoding: 'utf-8', flag: 'w' });
 }
 
 export async function sameCodeDetect(dirOfCwd?: string) {
@@ -87,4 +87,38 @@ export async function sameCodeDetect(dirOfCwd?: string) {
   const validContentList = nodeContentGroupList.filter(e => validDepthList.includes(e.depth)).sort((a, b) => b.depth - a.depth);
   const md = Core.createMarkdownFile(validContentList);
   writeFileSync(join(process.cwd(), `${dayjs().format('YYYYMDD_HHmm')}_same_code.md`), md, { encoding: 'utf-8', flag: 'w' });
+}
+
+export async function gitDiffDetect() {
+  const today = dayjs().format('YYYYMDD');
+  const { gitUrl, branchName } = await getGitRepositoryAndBranch();
+  logger.ready("准备生成临时工作目录...")
+  await execa.execa(`mkdir -p ${today}`, {shell: '/bin/bash'});
+  logger.info("临时目录建立完成");
+  logger.ready("准备clone源代码到临时目录下的target文件夹")
+  await execa.execa(`git clone ${gitUrl} ${today}/target`, {shell: '/bin/bash'});
+  logger.info("源代码clone完成");
+  logger.ready("准备clone源代码到临时目录下的source文件夹")
+  await execa.execa(`git clone ${gitUrl} ${today}/source`, {shell: '/bin/bash'});
+  logger.info("源代码clone完成");
+  logger.ready("准备切换到目标分支")
+  await execa.execa(`cd ${today}/target && git fetch origin ${branchName}:${branchName} && git checkout ${branchName}`, {shell: '/bin/bash'});
+  logger.info("分支切换完成");
+  logger.ready("准备生成git_diff.txt文件")
+  await execa.execa(`cd ${today}/target && git diff master..${branchName} --unified=0 --output=git_diff.txt`, {shell: '/bin/bash'});
+  logger.info("git_diff.txt文件生成完成");
+  logger.ready("准备生成插件文件");
+  writeFileSync(join(process.cwd(), today, 'target', 'plugin.ts'), pluginFileContent, { encoding: 'utf-8', flag: 'w' });
+  logger.info("插件文件生成完成");
+  logger.ready("准备生成报告");
+  await execa.execa(`cd ${today}/target && yarn install && npm run build`,  {shell: '/bin/bash'});
+  logger.info("报告生成完成！");
+  logger.ready("准备移动报告");
+  const content = readFileSync(join(process.cwd(), today, 'target', jsonName), "utf-8");
+  const mdFileName = `${dayjs().format('YYYYMDD_HHmm')}_${jsonName}`;
+  writeFileSync(join(process.cwd(), mdFileName), content, { encoding: 'utf-8', flag: 'w' });
+  logger.info("报告完成: " + mdFileName);
+  rimraf(join(process.cwd(), today), () => {
+    logger.info("临时目录已删除");
+  });
 }
